43035897100667c17d326e85c671eba3b54e1291
Oliver Caldwell <olliec87@gmail.com>
2016-06-14 21:26:13 +0100

    Document the mixins

--- bastion.conf.js
+++ bastion.conf.js
@@ -3,6 +3,11 @@
 export function webpack () {
   // I'm using pre-configured webpack mixins.
   // They mutate the config object for you.
+
+  // Adds React hot module reloading to the dev server.
   this.react()
+
+  // Adds first class Elm lang support (through normal JavaScript imports!)
+  // as well as hot module reloading.
   this.elm()
 }
